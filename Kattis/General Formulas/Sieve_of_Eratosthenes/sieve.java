e2987e9edec12eb2855d008c1799796ad684c3c8
samatbryan <samatbryan>
2019-09-20 09:28:39 -0700

    sieves"

---
+++ Kattis/General Formulas/Sieve_of_Eratosthenes/sieve.java
@@ -0,0 +1,17 @@
+void sieveOfEratosthenes(int n)
+    {
+        boolean prime[] = new boolean[n+1];
+        for(int i=0;i<n;i++)
+            prime[i] = true;
+
+        for(int p = 2; p*p <=n; p++)
+        {
+            // If prime[p] is not changed, then it is a prime
+            if(prime[p] == true)
+            {
+                // Update all multiples of p
+                for(int i = p*p; i <= n; i += p)
+                    prime[i] = false;
+            }
+        }
+    }
