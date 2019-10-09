4bcd94c10a8e7b7ec89ca2d6985764da162731c7
samatbryan <samatbryan>
2019-10-09 02:10:37 -0700

    bfs and dfs

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.ArrayDeque;
+import java.util.Iterator;
 // undirected graph
 public class Graph{
 
@@ -20,6 +21,7 @@ public class Graph{
     adj[u].add(v);
     adj[v].add(u);
   }
+  
   boolean hasEdge(int u, int v){
     int size = adj[u].size();
     for(int i=0; i<size; i++){
@@ -54,8 +56,29 @@ public class Graph{
   }
   void dfs(int source){
     boolean visited[] = new boolean[V];
+    visited[source] = true;
+    Iterator<Integer> i = adj[source].listIterator();
 
+    while(i.hasNext()){
+      int next = i.next();
+      if(!visited[next]){
+        explore(next, visited);
+      }
+    }
   }
+  void explore(int v, boolean[] visited){
+    System.out.println(v);
+    visited[v] = true;
+    Iterator<Integer> i = adj[v].listIterator();
+    while(i.hasNext()){
+      int next = i.next();
+      if(!visited[next]){
+        explore(next,visited);
+      }
+    }
+  }
+
+  //dijkstras (greedy algorithm)
   public static void main(String[] args){
     Graph g = new Graph(10);
     g.addEdge(0,1);
@@ -64,6 +87,6 @@ public class Graph{
     g.addEdge(2,4);
     g.addEdge(3,5);
     g.addEdge(4,6);
-    g.bfs(0);
+    g.dfs(0);
   }
 }
